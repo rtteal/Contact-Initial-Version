6b79e05c3b212347074afdb7821ff21f65003bc3
rtteal <rtaylorteal@yahoo.com>
2014-11-22 19:06:54 -0800

    Created method to serialize Contact to json.

--- Contact_Client/app/src/main/java/com/contact/model/Contact.java
+++ Contact_Client/app/src/main/java/com/contact/model/Contact.java
@@ -3,24 +3,23 @@ package com.contact.model;
 import android.util.Log;
 
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
+import org.apache.http.entity.StringEntity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
+ * Models the data associated with a contact.
  * Created by rtteal on 11/16/2014.
  */
 public class Contact implements Serializable {
     private static final long serialVersionUID = -8959832007991513854L;
     private String fName, lName, phone, email, photo;
 
-    private Contact(){}
+    public Contact(){}
 
     public static Contact fromJson(JSONObject jsonObject){
         Log.d("debug", "creating contact...");
@@ -40,23 +39,39 @@ public class Contact implements Serializable {
     }
 
     public static ArrayList<Contact> fromJson(JSONArray jsonArray){
-        Log.d("debug", "creating contacts...");
+        Log.d("fromJson", "creating contacts...");
         ArrayList<Contact> contacts = new ArrayList<Contact>();
         for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject contactObject = null;
             try {
-                contactObject = jsonArray.getJSONObject(i);
+                JSONObject contactObject = jsonArray.getJSONObject(i);
+                Contact contact = Contact.fromJson(contactObject);
+                if (contact != null) {
+                    contacts.add(contact);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
-                continue;
-            }
-
-            Contact contact = Contact.fromJson(contactObject);
-            if (contact != null) {
-                contacts.add(contact);
             }
         }
         return contacts;
+    }
+
+    public StringEntity getRequestParams(){
+        // TODO figure out how to store userNmme
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("userName", "taylor");
+            jsonParams.put("fName", fName);
+            jsonParams.put("lName", lName);
+            jsonParams.put("image", photo);
+            jsonParams.put("tel", phone);
+            jsonParams.put("email", email);
+            return new StringEntity(jsonParams.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public String getName() {
@@ -73,6 +88,26 @@ public class Contact implements Serializable {
 
     public String getPhoto() {
         return photo;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
     }
 
     @Override

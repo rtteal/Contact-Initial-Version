23b4116fc2278c6d640eeb56a35cf910e5cc8a00
rtteal <rtteal@gmail.com>
2014-11-25 19:13:54 -0800

    Passing photo to ViewProfileActivity.

--- Contact_Client/app/src/main/java/com/contact/views/EditProfileActivity.java
+++ Contact_Client/app/src/main/java/com/contact/views/EditProfileActivity.java
@@ -7,11 +7,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.contact.R;
 import com.contact.client.ContactClient;
 import com.contact.model.Contact;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -22,8 +24,10 @@ import org.json.JSONObject;
  */
 public class EditProfileActivity extends Activity {
     private EditText etFName, etLName, etPhone, etEmail;
+    private ImageView ivPhoto;
     private Button btSubmit;
     private Contact profile;
+    private Contact contact;
     public static final String PROFILE_KEY = "profile";
 
     @Override
@@ -34,8 +38,20 @@ public class EditProfileActivity extends Activity {
         etLName = (EditText) findViewById(R.id.etLName);
         etPhone = (EditText) findViewById(R.id.etPhone);
         etEmail = (EditText) findViewById(R.id.etEmail);
+        ivPhoto = (ImageView) findViewById(R.id.ivPhoto);
         btSubmit = (Button) findViewById(R.id.btSubmitProfile);
         createOnClickListener();
+        contact = (Contact)
+                getIntent().getSerializableExtra(ContactActivity.PROFILE_KEY);
+        loadContact(contact);
+    }
+
+    // Populate the data for the contact
+    public void loadContact(Contact contact) {
+        // Populate data
+        Picasso.with(this).load(contact.getPhoto()).
+                placeholder(R.drawable.images).
+                into(ivPhoto);
     }
 
     private void createOnClickListener() {
@@ -56,6 +72,7 @@ public class EditProfileActivity extends Activity {
         profile.setlName(etLName.getText().toString());
         profile.setPhone(etPhone.getText().toString());
         profile.setEmail(etEmail.getText().toString());
+        profile.setPhoto(contact.getPhoto());
         ContactClient.post("updateProfile", profile.getRequestParams(), new JsonHttpResponseHandler(){
             @Override
             public void onFailure(Throwable e, JSONObject errorResponse) {

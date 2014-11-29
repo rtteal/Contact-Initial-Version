4acc4c573292fb12a59174077ea003f52aa61856
rtteal <rtteal@gmail.com>
2014-11-29 10:19:01 -0800

    Creating initial unit test for CrontactActivity

---
+++ Contact_Client/app/src/androidTest/java/com/contact/views/tests/ContactActivityTest.java
@@ -0,0 +1,32 @@
+package com.contact.views.tests;
+
+import com.contact.views.ContactActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Unit test for ContactActivity.
+ * Created by rtteal on 11/26/2014.
+ */
+//@Config(manifest = "./app/src/main/AndroidManifest.xml")
+@RunWith(RobolectricTestRunner.class)
+public class ContactActivityTest {
+    private ContactActivity activity;
+
+    @Before
+    public void setup()  {
+        activity = Robolectric.buildActivity(ContactActivity.class).create().get();
+    }
+
+    @Test
+    public void checkActivityNotNull() throws Exception {
+        assertNotNull(activity);
+    }
+
+}
